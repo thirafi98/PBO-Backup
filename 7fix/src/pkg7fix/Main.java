b4b321bc5feb6be5aac9a209583d868b049fd6bd
Thirafi Wian <thirafi.badai@gmail.com>
2017-11-02 21:57:35 +0700

    perbaikan tp 7

---
+++ 7fix/src/pkg7fix/Main.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg7fix;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thirafi Wian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList <animal> ar = new ArrayList();
+        animal a1 = new animal(){
+            @Override
+            public void showName() {
+                System.out.println("Bird");
+            }
+
+            @Override
+            public void movc() {
+                System.out.println("Flying");
+            }
+            
+        };
+        animal a2 = new animal(){
+            @Override
+            public void showName() {
+                System.out.println("Cat");
+            }
+
+            @Override
+            public void movc() {
+                System.out.println("Walking");
+            }
+            
+        };
+        animal a3 = new animal(){
+            @Override
+            public void showName() {
+                System.out.println("Fish");
+            }
+
+            @Override
+            public void movc() {
+                System.out.println("Swimming");
+            }
+            
+        };
+        
+        ar.add(a1);
+        ar.add(a2);
+        ar.add(a3);
+        for (animal s:ar){
+            s.showName();
+            s.movc();
+        }
+    }
+    
+}
